a57f7cba6cc9c6b34776841c63cd7d5280883443
ThatIndieDev <thatindiedev@gmail.com>
2016-03-25 19:58:17 +0100

    Added files via upload

---
+++ scan.py
@@ -0,0 +1,37 @@
+import sys
+import base
+'''
+List of IDs:
+0: air
+1: dirt
+2: grass
+3: stone
+4: wood
+5: wood planks
+6: furnace(off)/6.1: furnace(on)
+'''
+
+#vars
+0 = air
+1 = dirt
+2 = grass
+3 = stone
+4 = wood
+5 = wood planks
+6 = furnaceoff
+6.1 = furnaceon
+
+ 
+#defining functions
+def findBlock(x, y, z):
+    temp = 0
+def mainloop
+    #main-loop
+    while True:
+        x = base.x
+        y = base.y
+        z = base.z
+        
+    
+    
+    
